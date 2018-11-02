2a7f3f5f74858e02e0cfa9ca3758e341ca7a4ccd
joonspoon <joonspoon@joonspoon.com>
2018-11-02 11:19:33 -0700

    Helper script to upload training images from Zumi to Google Drive

---
+++ sample/deep-learning-demos/driving/SaveImagesToCloud.py
@@ -0,0 +1,5 @@
+#python3 /home/pi/zumi/sample/deep-learning-demos/driving/SaveImagesToCloud.py --noauth_local_webserver
+
+import TrainingDataHelper as data_helper
+
+data_helper.upload_images_to_cloud()
