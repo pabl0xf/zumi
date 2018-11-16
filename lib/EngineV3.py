134fed28e60714bee6e9da1bb515221d51e21cb2
joonspoon <joonspoon@joonspoon.com>
2018-11-15 18:15:05 -0800

    Getting engine to work for V3.

--- lib/EngineV3.py
+++ lib/EngineV3.py
@@ -1,7 +1,7 @@
 #Import this file to use all of the functions necessary to move Linky
 import time
 import sys
-sys.path.insert(0,'/home/pi/zumi/src')
+sys.path.insert(0,'/home/pi/zumi/src/ZumiV3/')
 import ZumiV3 as zumi
 
 def go_forward():
