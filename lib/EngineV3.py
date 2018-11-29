1b93fd89f3b7ec9e0cd8f8d59f6ccbc04b148ae7
joonspoon <joonspoon@joonspoon.com>
2018-11-29 11:55:05 -0800

    Adapted Engine for deep learning.

--- lib/EngineV3.py
+++ lib/EngineV3.py
@@ -14,31 +14,22 @@ def go_backward():
     zumi.backward()
 
 def right_a_bit():
-#     zumi.setSpeed(30)
     zumi.right()
     time.sleep(.1)
-#     zumi.stop()
 
 def left_a_bit():
-#     zumi.setSpeed(30)
     zumi.left()
     time.sleep(.1)
-#     zumi.stop()
 
 def forward_a_bit():
-    zumi.setSpeed(40)
     zumi.forward()
-    time.sleep(.8)
-    zumi.stop()
+    time.sleep(.6)
 
 def set_speed(s):
     zumi.setSpeed(s)
-    print("Speed = "+str(s))
 
-def keep_turning_left():
+def left():
     zumi.left()
-#     print("Left")
 
-def keep_turning_right():
+def right():
     zumi.right()
-#     print("Right")
