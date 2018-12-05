4acaa51251513fca124a76e61495dd6b87b1323c
joonspoon <joonspoon@joonspoon.com>
2018-12-04 16:00:25 -0800

    Added instruction for setting permissions for credentials file.

--- sample/deep-learning-demos/driving/SaveImagesToCloud.py
+++ sample/deep-learning-demos/driving/SaveImagesToCloud.py
@@ -3,3 +3,5 @@
 import TrainingDataHelper as data_helper
 
 data_helper.upload_images_to_cloud()
+
+# If this script gives an error that it can't find credentials.json, you should change it's permissions with chmod 777 credentials.json
