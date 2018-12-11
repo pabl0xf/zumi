551f68f8faeb1958e1f0d266e61e5217eb99c9eb
joonspoon <joonspoon@joonspoon.com>
2018-12-10 18:21:03 -0800

    Fixing up Camera functions for Blockly.

--- lib/Camera.py
+++ lib/Camera.py
@@ -4,7 +4,7 @@ from TawnCam import PiCamera
 import cv2
 
 print("loading camera...")
-camera = PiCamera(image_w=400, image_h=400, image_d=3, framerate=10)
+camera = PiCamera(image_w=300, image_h=300, image_d=3, framerate=10)
 
 def face_detected():
     face_detector = cv2.CascadeClassifier('/home/pi/zumi/sample/openCV-demos/face_detection/haarcascade_frontalface_default.xml')
@@ -13,24 +13,9 @@ def face_detected():
     faces = face_detector.detectMultiScale(image, 1.3, 5)
     return type(faces) is not tuple
 
-# def face_detected_old():
-    
-#     face_detector = cv2.CascadeClassifier('/home/pi/zumi/sample/haarcascade_frontalface_default.xml')
-        
-# #     with .PiCamera() as camera:
-#         with picamera.array.PiRGBArray(camera) as stream:
-#             camera.capture(stream, format='bgr')
-#             image = stream.array
-
-# #             gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) might work better with this?
-
-#             faces = face_detector.detectMultiScale(image, 1.3, 5)
-
-#             return type(faces) is not tuple
-
 def take_photo():
     image = camera.run()
-    cv2.imwrite(image, "your-face.jpg")
+    cv2.imwrite("your-face.jpg", image)
     show();
         
 def show():
