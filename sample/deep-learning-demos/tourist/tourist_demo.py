2123cd8da011bff9df72755d977b2f27a85afa36
joonspoon <joonspoon@joonspoon.com>
2018-12-20 14:13:08 -0800

    Michael's classification code integrated with driving.

---
+++ sample/deep-learning-demos/tourist/tourist_demo.py
@@ -0,0 +1,71 @@
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import sys
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Activation, Flatten, Dense, Conv2D, MaxPooling2D
+from keras.preprocessing.image import array_to_img
+from keras import backend as K
+import time
+import TouristDemoHelper as tourist_demo
+drive_model = tourist_demo.load_model("landmark")
+
+WIDTH = 64
+HEIGHT = 64
+
+if K.image_data_format() == 'channels_first':
+	input_shape = (3, WIDTH, HEIGHT)
+else:
+	input_shape = (WIDTH, HEIGHT, 3)
+
+model = Sequential()
+
+model.add(Conv2D(32, (3, 3), input_shape=input_shape))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(32, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())
+
+model.add(Dense(128))
+model.add(Activation('relu'))
+model.add(Dense(2))
+model.add(Activation('sigmoid'))
+model.load_weights('drawing_weights.hdf5')
+
+model.compile(loss='categorical_crossentropy',
+	optimizer='adam',
+	metrics=['accuracy'])
+
+camera = PiCamera()
+camera.resolution = (WIDTH, HEIGHT)
+camera.hflip = True
+camera.vflip = True
+camera.framerate = 32
+rawCapture = PiRGBArray(camera, size=(WIDTH, HEIGHT))
+
+time.sleep(0.1)
+
+null = input("Press enter to begin!")
+
+try:
+	for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+		x = frame.array
+		maximum = x.max()
+		scaledValue = 1.0
+		if maximum > 0:
+			scaledValue = 255.0/maximum
+		y = scaledValue * x
+		y = np.expand_dims(y, axis=0)
+		pred = model.predict(y)
+		print(pred)
+		rawCapture.truncate(0)
+		camera.close()
+		#Decide whether 'nyc' or 'china' based on pred
+		tourist_demo.drive_to_landmark("china", drive_model, camera)
+
+except KeyboardInterrupt:
+		print("\nExiting...")
