d6f1d295e2fc999d2d98e4367ca8fcae3bf12b13
joonspoon <joonspoon@joonspoon.com>
2018-12-20 22:29:26 -0800

    Adjusted speeds for Pumi and Whumi when demo-ing.

--- sample/deep-learning-demos/tourist/drive_demo.py
+++ sample/deep-learning-demos/tourist/drive_demo.py
@@ -1,5 +1,5 @@
 import TouristDemoHelper as tourist_demo
 model = tourist_demo.load_model("landmark")
 
-tourist_demo.speed = 40
+tourist_demo.speed = 40  #35 for Pumi
 tourist_demo.drive_to_landmark("china", model)
