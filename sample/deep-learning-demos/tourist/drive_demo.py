4947eb35f319aa9e67d7e009f2b61ae1c37e1875
joonspoon <joonspoon@joonspoon.com>
2018-12-20 22:56:04 -0800

    Added interaction with user for Zumi destination.

--- sample/deep-learning-demos/tourist/drive_demo.py
+++ sample/deep-learning-demos/tourist/drive_demo.py
@@ -1,5 +1,13 @@
+print("Zumi likes to travel! She can go to 'eiffel', 'china', or 'nyc'. ")
+print("Press ctrl+C to choose a new destination. Loading will take a minute...") 
+
 import TouristDemoHelper as tourist_demo
 model = tourist_demo.load_model("landmark")
-
 tourist_demo.speed = 40  #35 for Pumi
-tourist_demo.drive_to_landmark("china", model)
+
+while True:
+    try:
+        destination = input("where would you like to go?")
+        tourist_demo.drive_to_landmark(destination, model)
+    except: #catch a keyboard interrupt
+        print("hi")
