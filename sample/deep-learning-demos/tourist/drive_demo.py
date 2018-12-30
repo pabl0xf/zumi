ab766fcb7d7dc6cf058883fad31d5992a198937b
joonspoon <joonspoon@joonspoon.com>
2018-12-29 20:08:31 -0800

    Added calibration at the beginning of landmark run.

--- sample/deep-learning-demos/tourist/drive_demo.py
+++ sample/deep-learning-demos/tourist/drive_demo.py
@@ -3,11 +3,11 @@ print("Press ctrl+C to choose a new destination. Loading will take a minute...")
 
 import TouristDemoHelper as tourist_demo
 model = tourist_demo.load_model("landmark")
-tourist_demo.speed = 40  #35 for Pumi
+tourist_demo.calibrate_motors()
 
 while True:
     try:
         destination = input("where would you like to go?")
         tourist_demo.drive_to_landmark(destination, model)
-    except: #catch a keyboard interrupt
-        print("hi")
+    except Exception as e:  #catch a keyboard interrupt
+        print(e)
