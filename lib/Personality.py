2233f6b5e503b58ed52603072be959de98b16d66
joonspoon <joonspoon@joonspoon.com>
2018-12-29 18:46:18 -0800

    Updated Personality functions with eyes.

--- lib/Personality.py
+++ lib/Personality.py
@@ -1,45 +1,54 @@
-import time
 import sys
 sys.path.insert(0,'/home/pi/zumi/lib')
 import Engine as engine
-import Infrared as infrared
 import Speaker as speaker
+import Lights as lights
+import Eyes as eyes
+import Timer as timer
 
 def happy_zumi():
 #     speaker.play_happy_sound()
+    eyes.happy()
+    lights.turn_on()
     time.sleep(.5)
     engine.set_speed(30)
-    engine.turn_left()
-    engine.turn_right()
-    engine.turn_left()
-    engine.turn_right()
-    engine.turn_left()
-    engine.turn_right()
+    engine.left_a_bit()
+    engine.right_a_bit()
+    engine.left_a_bit()
+    engine.right_a_bit()
+    engine.left_a_bit()
+    engine.right_a_bit()
+    lights.turn_off()
     engine.stop()
 #     speaker.play_happy_sound()
     
-import Linky_SerialCom as ropi 
-
 def sad_zumi():
+    sys.path.insert(0,'/home/pi/zumi/src/ZumiV3/')
+    import ZumiV3 as zumi
+
     baseline_speed = 15
-    speed_difference = 10
-    curve_duration = .5
+    speed_difference = 20
+    curve_duration = .7
+    
+    eyes.sad()
 
-    ropi.setMotor(-baseline_speed+speed_difference,-baseline_speed)
+    faster = baseline_speed+speed_difference
+    slower = baseline_speed
+    zumi.setMotor(-faster, -slower)
     time.sleep(curve_duration)
-    ropi.setMotor(-baseline_speed,-baseline_speed+speed_difference)
+    zumi.setMotor(-slower, -faster)
     time.sleep(curve_duration)
-
-    ropi.setMotor(-baseline_speed+speed_difference,-baseline_speed)
-    time.sleep(curve_duration)
-    ropi.setMotor(-baseline_speed,-baseline_speed+speed_difference)
-    time.sleep(curve_duration)
-
     engine.stop()
-    speaker.play_sad_sound()
+   
+#     speaker.play_sad_sound()
     
 def excited_zumi():
-    engine.set_speed(45)
-    engine.turn_right()
-    time.sleep(1)
+    eyes.hello()
+    engine.set_speed(100)
+    engine.right()
+    timer.wait(1.6)
     engine.stop()
+    eyes.excited()
+    eyes.hello()
+    
+excited_zumi()
