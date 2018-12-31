8d9920c71ac04fd9626c01ca45f6a15f3475e4e2
joonspoon <joonspoon@joonspoon.com>
2018-12-30 22:26:01 -0800

    Renamed model for driving with Fictiv assembly.

--- sample/deep-learning-demos/tourist/drive_demo.py
+++ sample/deep-learning-demos/tourist/drive_demo.py
@@ -2,7 +2,7 @@ print("Zumi likes to travel! She can go to 'eiffel', 'china', or 'nyc'. ")
 print("Press ctrl+C to choose a new destination. Loading will take a minute...") 
 
 import TouristDemoHelper as tourist_demo
-model = tourist_demo.load_model("landmark")
+model = tourist_demo.load_model("driving")
 tourist_demo.calibrate_motors()
 
 while True:
