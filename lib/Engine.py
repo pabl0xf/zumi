64a26b7f1915a91aaf494214149bb423267dd47c
joonspoon <joonspoon@joonspoon.com>
2018-12-30 22:19:22 -0800

    Adding calibration to Engine. Needs work later....

--- lib/Engine.py
+++ lib/Engine.py
@@ -7,7 +7,7 @@ import ZumiV3 as zumi
 speed = 50
 right_faster_by = 0
 left_faster_by = 0
-zumi.enableAcceleration()
+zumi.disableAcceleration()
 
 def set_right_faster_by(percentage_increase):
     global right_faster_by
@@ -30,19 +30,26 @@ def stop():
 def reverse():
     zumi.backward()
 
-def right_a_bit():
+def right_a_bit_OLD():
     zumi.right()
     time.sleep(.1)
+    
+def right_a_bit():   
+    left_speed = speed
+    right_speed = speed
+    right_speed = speed + speed/100*right_faster_by
+    left_speed = speed + speed/100*left_faster_by
+    zumi.engage_motors(right_speed, -left_speed)
+    time.sleep(.1)
 
-def left_a_bit(robot_name="zumi"):
-    zumi.left()
-    if robot_name == "whumi":
-        time.sleep(.2)
-    elif robot_name =="pumi":
-        time.sleep(.2)
-    else:
-        time.sleep(.1)
-
+def left_a_bit():
+    left_speed = speed
+    right_speed = speed
+    right_speed = speed + speed/100*right_faster_by
+    left_speed = speed + speed/100*left_faster_by
+    zumi.engage_motors(-right_speed, left_speed)
+    time.sleep(.1)
+        
 def forward_a_bit():
     forward()
     time.sleep(.6)
@@ -61,3 +68,18 @@ def left():
 
 def right():
     zumi.right()
+    
+def calibrate_motors_for_tourist_demo():
+    robots = {"whumi": {"left":0, "right":28},
+          "newmi": {"left":0, "right":20},
+          "pumi": {"left":-22, "right":-15},
+          "kickmi": {"left":0, "right":20},
+         }
+    set_speed(70)
+    set_left_faster_by(robots.get(get_robot_name()).get("left"))
+    set_right_faster_by(robots.get(get_robot_name()).get("right"))
+    
+def get_robot_name():
+    import socket
+    hostname = socket.gethostname() 
+    return hostname
